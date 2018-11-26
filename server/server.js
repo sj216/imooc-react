534f5c09b7bab295af757f6742bebf55261bc7f1
sj216 <786212822@Qq.com>
2018-11-26 23:15:16 +0800

    socket.io简单上手操作 shijun 2018/11/26

--- server/server.js
+++ server/server.js
@@ -4,10 +4,23 @@ const cookieParser = require('cookie-parser')
 const userRouter = require('./user')
 
 const app = express()
+// work with express
+const server = require('http').Server(app)
+
+const io = require('socket.io')(server); // io 和 express 进行关联
+
+io.on('connection', function (socket) {
+  console.log('user login')
+  socket.on('sendmsg', function (data) {
+    console.log(data);
+    io.emit('recvmsg', data)
+  })
+})
+
 app.use(cookieParser())
 app.use(bodyParser.json())
 app.use('/user', userRouter); //开启一个中间件
 
-app.listen(9093, function () {
+server.listen(9093, function () {
   console.log('Node app start at port 9093')
 })
--- src/component/chat/chat.js
+++ src/component/chat/chat.js
@@ -1,9 +1,54 @@
 import React from 'react'
+import io from 'socket.io-client'
+import {InputItem, List} from 'antd-mobile'
+import {login} from "../../redux/user.redux";
+
+const socket = io('ws://localhost:9093')
 
 class Chat extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '',
+      msg: []
+    }
+  }
+
+  componentDidMount() {
+    socket.on('recvmsg', (data) => {
+      this.setState({
+        msg: [...this.state.msg, data.text]
+      })
+    })
+  }
+
+  handleSubmit() {
+    socket.emit('sendmsg', {text: this.state.text})
+    this.setState({text: ''})
+  }
+
   render() {
     return (
-      <h2>Chat width user {this.props.match.params.user}</h2>
+      <div>
+        {this.state.msg ? this.state.msg.map(v => {
+          return <p key={v}>{v}</p>
+        }) : null
+        }
+        <div className="stick-footer">
+          <List>
+            <InputItem
+              placeholder='请输入'
+              value={this.state.text}
+              onChange={v => {
+                this.setState({
+                  text: v
+                })
+              }}
+              extra={<span onClick={() => this.handleSubmit()}>发送</span>}
+            />
+          </List>
+        </div>
+      </div>
     )
   }
 }
